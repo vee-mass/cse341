79db787cabff48e23d14d5cab1db72945ccf2401
vee-mass <vmassela@gmail.com>
2026-03-09 09:14:31 +0200

    git ignore file update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,11 @@
 const routes = require('express').Router(); 
-const contactsController = require('../controllers/contacts'); // Import the new contacts controller
+const contactsController = require('../controllers/contacts'); 
 
-routes.get('/contacts', contactsController.getAll);
+routes.get('/', (req, res) => { 
+  res.send("Hello World"); 
+});
 
-routes.get('/contacts/:id', contactsController.getSingle);
+routes.get('/contacts', contactsController.getAll); 
+routes.get('/contacts/:id', contactsController.getSingle); 
 
-module.exports = routes;
+module.exports = routes; 
